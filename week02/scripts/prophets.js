c4c767f064e94beb8751ab21d84949f8643e19ab
cesarperez117 <cperezpineda@byupathway.edu>
2026-05-18 21:02:45 -0400

    prophets fetch learning completed

---
+++ week02/scripts/prophets.js
@@ -0,0 +1,45 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function getProhetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProhetData()
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+    
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        //let death = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        //death.textContent = `Date of Death: ${prophet.death}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name}lastname`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        //card.appendChild(death);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    }); 
+}
